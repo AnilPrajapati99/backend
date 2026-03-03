3566c852936c018d02b7cc0acc3487ad73597e0a
Anil Prajapati <anilofficial62@gmail.com>
2026-03-03 22:33:35 +0530

    project making

---
+++ IgProject/src/controllers/auth.controlers.js
@@ -0,0 +1,106 @@
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const userModel = require("../model/user.model");
+
+// const { profile } = require("console");
+
+// Register
+
+async function registerController(req, res) {
+  const { username, email, password, bio, profileImage } = req.body;
+
+  const isUserAlredyExits = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (isUserAlredyExits) {
+    return res.status(409).json({
+      message:
+        "User is Alredy Exits" +
+        (isUserAlredyExits.email == email
+          ? "Email is Alredy Exits"
+          : "Username is Alredy Exits"),
+    });
+  }
+
+  const hash = crypto.createHash("md5").update(password).digest("hex");
+
+  const userdata = await userModel.create({
+    username,
+    email,
+    password: hash,
+    bio,
+    profileImage,
+  });
+
+  const token = jwt.sign({ id: userdata._id }, process.env.Token, {
+    expiresIn: "1d",
+  });
+
+  res.cookie("JWT_TOKEN", token);
+
+  res.status(200).json({
+    message: "User Create SuccessFully",
+    user: {
+      email: userdata.email,
+      username: userdata.username,
+      bio: userdata.bio,
+      profile: userdata.profileImage,
+    },
+    token,
+  });
+}
+
+// Login
+
+async function loginController(req, res) {
+  const { email, password, username } = req.body;
+
+  // LOgin email with password username with password
+
+  const user = await userModel.findOne({
+    $or: [{ username: username }, { email: email }],
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User Not Exits",
+    });
+  }
+
+  const hash = crypto.createHash("md5").update(password).digest("hex");
+
+  const isPasswordMatch = hash === user.password;
+
+  if (!isPasswordMatch) {
+    return res.status(404).json({
+      message: "password is invalid",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+
+    process.env.Token,
+    { expiresIn: "1d" },
+  );
+
+  res.cookie("Token", token);
+
+  res.status(200).json({
+    message: "User Login SuccessFully",
+    user: {
+      username: user.username,
+      email: user.email,
+      bio: user.bio,
+      profileImage: user.profileImage,
+    },
+  });
+}
+
+module.exports = {
+  registerController,
+  loginController,
+};
