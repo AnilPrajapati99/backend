b952b7965c3dbe7ed02ed0b3a12ae25a7325afc8
Anil Prajapati <anilofficial62@gmail.com>
2026-03-07 15:10:53 +0530

    imagekit

--- IgProject/src/controllers/post.controler.js
+++ IgProject/src/controllers/post.controler.js
@@ -1,15 +1,16 @@
 const postModel = require("../model/post.model");
-const imageKit = require("@imagekit/nodejs");
+const ImageKit = require("@imagekit/nodejs");
+require("dotenv").config();
 
-// const imagekit = new imageKit({
-//   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-// });
-console.log(process.env.IMAGEKIT_PRIVATE_KEY);
+const imagekit = new ImageKit({
+  privatekey: process.env.ImageKit_Key,
+});
+// console.log(process.env.IMAGEKIT_PRIVATE_KEY);
 async function postCreate(req, res) {
   console.log(req.body, req.file);
 
   const file = await imagekit.file.upload({
-    file: await imageKit.toFile(Buffer.from(req.file.buffer), "file"),
+    file: await ImageKit.toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "test",
   });
   res.send(file);
